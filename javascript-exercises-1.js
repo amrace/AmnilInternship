c202a4639dbed36c2564ae2b95fad78c78cd07b8
amrace <amrace321@gmail.com>
2021-08-12 13:35:08 +0545

    COM#3- Exercise completed.

--- javascript-exercises-1.js
+++ javascript-exercises-1.js
@@ -9,5 +9,86 @@
      console.log(word1.checkPalindrome()); 
      console.log(word2.checkPalindrome()); 
 
+//Exercise Two
+ Number.prototype.cube = function (num){
+   return num **3;
+ };
+ let inputNumber = new Number();
+ console.log(`${inputNumber.cube(3)}`);
+
+
+//Exercise Three
+  let providedArray = [
+    {id: 1, fullName:'John Doe'}, 
+    {id: 2, fullName:'Jim Doe'}, 
+    {id: 3, fullName:'Jane Doe'}
+  ];
+  //Creating reducer function
+  function arrayToObject(accumulater, currentElement){
+    return {...accumulater,[currentElement.id] : currentElement}
+  }
+  const objectArray = providedArray.reduce(arrayToObject, {});
+  console.log(objectArray);
+
+  //Exercise Four
+  let providedObject = {
+    1: { fullName:'John Doe'}, 
+    2: {fullName:'Jim Doe'}, 
+    3: {fullName:'Jane Doe'} 
+  };
+  let array = Object.values(providedArray);
+  console.log(array);
+
+  //Exercise Five
+const points = { 
+  p1: [2, 3], 
+  p2: [6, 7],
+
+  get displacement(){
+    let displacement1 = Math.sqrt((points.p2[0]-points.p1[0]**2) + (points.p2[1]-points.p1[1])**2);
+    return displacement1;
+  }
+}; 
+console.log(points.displacement);
+
+//Exercise Six
+function Person(person_fisrtName, person_lastName, person_dob){
+  this.firstName= person_fisrtName;
+  this.lastName= person_lastName;
+  this.dob= person_dob;
+
+  this.fullName= function(){
+    return(`${this.firstName} ${this.lastName}`);
+  }
+}
+const person = new Person("Jhon","Doe","1996-03-27");   
+person.age=function(){
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const month = day * 30;
+  const year = month * 12;
+  const currentDate = new Date().valueOf();
+  let currentAge=(currentDate - new Date(person.dob)) / year;
+  let noOfDays = (currentAge - Math.floor(currentAge)) * 365;
+  return(`age: ${currentAge.toFixed()} days: ${noOfDays.toFixed()}` );
+}
+  console.log(person.fullName());
+  console.log(person.age());
+
+  //Exercise Seven
+  function printRecursive(num){
+    console.log(num);
+    const number = num -1;
+    if(number>=0){
+        printRecursive(number);
+    }if(number>=0){
+      printRecursive(-number);
+    }
+  }
+  printRecursive(5);
+  printRecursive(-5);
+
 
 
